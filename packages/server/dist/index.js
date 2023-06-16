8f97cbae77ca75ae06dcfe4177e5c52d5d7fceb5
George Oastler <goastler4@gmail.com>
2023-06-16 14:02:01 +0100

    api for html and server info

--- packages/server/dist/index.js
+++ packages/server/dist/index.js
@@ -6,6 +6,22 @@ var port = Number(process.env.PORT || 3000);
 app.get('/', function (req, res) {
     res.send('Express + TypeScript Server');
 });
-app.listen(port, function () {
+app.get('/info', function (req, res) {
+    fetch('https://api.ipify.org').then(function (res) { return res.text(); }).then(function (serverIp) {
+        var info = {
+            clientIp: req.ip,
+            serverIp: serverIp
+        };
+        res.send(info);
+    }).catch(function (err) {
+        res.status(500).send(err);
+    });
+});
+var listener = app.listen(port, function () {
     console.log("Server is running at http://localhost:".concat(port));
+});
+process.on('SIGINT', function () {
+    console.log('Bye bye!');
+    listener.close();
+    process.exit();
 });
--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -10,6 +10,24 @@ app.get('/', (req: Request, res: Response) => {
   res.send('Express + TypeScript Server');
 });
 
-app.listen(port, () => {
+app.get('/info', (req: Request, res: Response) => {
+    fetch('https://api.ipify.org').then(res => res.text()).then(serverIp => {
+        const info = {
+            clientIp: req.ip,
+            serverIp
+        }
+        res.send(info);
+    }).catch(err => {
+        res.status(500).send(err);
+    });
+});
+
+const listener = app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
+});
+
+process.on('SIGINT', () => {
+    console.log('Bye bye!');
+    listener.close();
+    process.exit();
 });
